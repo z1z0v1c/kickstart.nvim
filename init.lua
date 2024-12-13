5d958a7e6e8ba1ef7d4998ea06869a20b8f064ad
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2024-12-13 19:26:08 +0100

    install and use kanagawa theme

--- init.lua
+++ init.lua
@@ -836,7 +836,9 @@ require('lazy').setup({
       }
     end,
   },
-
+  {
+    'rebelot/kanagawa.nvim',
+  },
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -972,7 +974,34 @@ require('lazy').setup({
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
-vim.cmd 'colorscheme retrobox'
+-- Default options:
+require('kanagawa').setup {
+  compile = false, -- enable compiling the colorscheme
+  undercurl = true, -- enable undercurls
+  commentStyle = { italic = true },
+  functionStyle = {},
+  keywordStyle = { italic = true },
+  statementStyle = { bold = true },
+  typeStyle = {},
+  transparent = false, -- do not set background color
+  dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+  terminalColors = true, -- define vim.g.terminal_color_{0,17}
+  colors = { -- add/modify theme and palette colors
+    palette = {},
+    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+  },
+  overrides = function(colors) -- add/modify highlights
+    return {}
+  end,
+  theme = 'dragon', -- Load "wave" theme when 'background' option is not set
+  background = { -- map the value of 'background' option to a theme
+    dark = 'dragon', -- try "dragon" !
+    light = 'lotus',
+  },
+}
+
+-- setup must be called before loading
+vim.cmd 'colorscheme kanagawa'
 
 vim.api.nvim_create_augroup('neotree', {})
 vim.api.nvim_create_autocmd('VimEnter', {
